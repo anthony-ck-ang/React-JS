7edacead03c5a2e5457dc39bcda0e053049837e5
Anthony Ang <anthonyang25@gmail.com>
2019-11-13 17:57:44 +0800

    Remove API key

--- handle_userInput_form_events_apiReq_refactor_imgList_v1.2/src/api/unsplash.js
+++ handle_userInput_form_events_apiReq_refactor_imgList_v1.2/src/api/unsplash.js
@@ -5,6 +5,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID db5626aaf8cff3cc07e758a149030775d432644e8f2fe51909e8a6895361f962"
+      " "
   }
 });
